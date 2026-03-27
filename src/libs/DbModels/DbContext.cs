67e0ff0c057d32179feabe9a608bcb7e1e5b0281
simeon <simmeon.nv@proton.me>
2026-03-27 10:57:40 +0200

    migrations for auth

--- src/libs/DbModels/DbContext.cs
+++ src/libs/DbModels/DbContext.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 namespace DbModels;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) 
+    : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<Example> Examples => Set<Example>();
 }
--- src/libs/DbModels/Migrations/20260327085317_Auth.Designer.cs
+++ src/libs/DbModels/Migrations/20260327085317_Auth.Designer.cs
@@ -4,6 +4,7 @@ using DbModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbModels.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260327085317_Auth")]
+    partial class Auth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
